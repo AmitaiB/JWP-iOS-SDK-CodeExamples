47d15fb09463aa0785c4950f8aadbbcb25a67542
AmitaiB <blickstein@gmail.com>
2019-06-04 22:55:47 -0400

    Initial Commit

---
+++ JWP-iOS-SDK-CodeExamples/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JWP-iOS-SDK-CodeExamples
+//
+//  Created by Amitai Blickstein on 6/4/19.
+//  Copyright © 2019 Amitai Blickstein. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
